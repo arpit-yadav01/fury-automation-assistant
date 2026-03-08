bdb6623f148d533b718ba6fe934eb065bacb479e
arpit-yadav01 <arpityadav170402@gmail.com>
2026-03-08 23:43:37 +0530

    code generator

---
+++ developer/code_generator.py
@@ -0,0 +1,9 @@
+# developer/code_generator.py
+
+def generate_code(language, task):
+
+    if language == "python" and task == "hello_world":
+
+        return """print("Hello World")"""
+
+    return ""
