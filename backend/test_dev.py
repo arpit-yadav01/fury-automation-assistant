f8d0d79098bab4631f15112903420bf58127066a
arpit-yadav01 <arpityadav170402@gmail.com>
2026-03-14 00:29:48 +0530

    test dev workflow

--- backend/test_dev.py
+++ backend/test_dev.py
@@ -4,9 +4,17 @@ from execution.executor import execute_plan
 plan = [
 
     {"intent": "open_vscode"},
+
     {"intent": "create_code_file", "filename": "hello.py"},
-    {"intent": "write_code", "code": 'print("Hello from Fury")'},
+
+    {
+        "intent": "write_code",
+        "code": 'print("Hello from Fury")'
+    },
+
     {"intent": "save_file"},
+
 ]
+
 
 execute_plan(plan)
