77a7cdccf019777305c0e4f83dbb081afa30c3c6
arpit-yadav01 <arpityadav170402@gmail.com>
2026-03-25 13:28:51 +0530

    fix the register agent , and skill agent with api

--- backend/agents/skill_agent.py
+++ backend/agents/skill_agent.py
@@ -1,7 +1,43 @@
-# agents/skill_agent.py
+# # agents/skill_agent.py
+
+# from agents.base_agent import BaseAgent
+
+# from skills.skill_manager import execute_skill
+
+
+# class SkillAgent(BaseAgent):
+
+#     def __init__(self):
+#         super().__init__("SkillAgent")
+
+#     # -------------------------
+
+#     def can_handle(self, task):
+
+#         if not isinstance(task, dict):
+#             return False
+
+#         if task.get("intent"):
+#             return True
+
+#         return False
+
+#     # -------------------------
+
+#     def handle(self, task):
+
+#         intent = task.get("intent")
+
+#         if not intent:
+#             return
+
+#         ok = execute_skill(task)
+
+#         if not ok:
+#             raise Exception("Skill failed")
+
 
 from agents.base_agent import BaseAgent
-
 from skills.skill_manager import execute_skill
 
 
@@ -10,26 +46,27 @@ class SkillAgent(BaseAgent):
     def __init__(self):
         super().__init__("SkillAgent")
 
-    # -------------------------
-
     def can_handle(self, task):
 
         if not isinstance(task, dict):
             return False
 
-        if task.get("intent"):
-            return True
-
-        return False
-
-    # -------------------------
-
-    def handle(self, task):
-
         intent = task.get("intent")
 
         if not intent:
-            return
+            return False
+
+        # FIX-2 do not steal code
+        if intent == "generate_code":
+            return False
+
+        # do not steal dev
+        if intent == "dev":
+            return False
+
+        return True
+
+    def handle(self, task):
 
         ok = execute_skill(task)
 
